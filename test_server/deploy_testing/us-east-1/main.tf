50f75189cfa3bb08aa9e4b2cef48fff55723ca3f
SZA <szaforward2@gmail.com>
2022-01-24 14:53:43 -0600

    fix deploy testing

--- test_server/deploy_testing/us-east-1/main.tf
+++ test_server/deploy_testing/us-east-1/main.tf
@@ -1,3 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::656306611222:role/terraform_atlantis"
+  }
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "hella-buckets-213213123121"
+    key    = "test_server/deploy-testing/us-east-1/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 module "server" {
     source        = "../../modules/server"
 }
